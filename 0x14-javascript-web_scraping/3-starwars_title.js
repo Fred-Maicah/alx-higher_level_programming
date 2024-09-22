5e3da00a95d724e6ca3244758e3e9d87ced1241c
fred maicah <fredmaicah@gmail.com>
2024-09-22 14:17:01 +0000

    My 0x14. JavaScript - Web scraping commit 2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,6 +6,12 @@ const request = require('request');
 // Get the movie ID from the command-line arguments
 const movieId = process.argv[2];
 
+// Ensure a movie ID was provided
+if (!movieId) {
+  console.error('Please provide a movie ID as an argument.');
+  process.exit(1); // Exit with an error code
+}
+
 // Construct the URL for the Star Wars API using the movie ID
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
@@ -13,11 +19,22 @@ const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request.get(url, (error, response, body) => {
   // If an error occurred during the request, log it to the console
   if (error) {
-    console.log(error);
-  } else {
-    // Parse the JSON response body
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+
+  // Check if the response status code is successful (200 OK)
+  if (response.statusCode !== 200) {
+    console.error(`Error: Received status code ${response.statusCode}`);
+    return;
+  }
+
+  // Try parsing the JSON response body
+  try {
     const data = JSON.parse(body);
     // Log the title of the movie to the console
     console.log(data.title);
+  } catch (parseError) {
+    console.error('Error: Unable to parse response as JSON.');
   }
 });
